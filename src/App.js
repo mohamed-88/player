e9ff3db70178743a927ed7a9abe44e5043e91df0
“mohamed-88” <mohamederedni4@gmail.com>
2025-01-09 16:47:30 +0300

    Domain

--- src/App.js
+++ src/App.js
@@ -166,7 +166,7 @@ function App() {
         <p className="text">
 	Copyright&copy;2025 All Rights Reserved By:<a target="_blank" href="https://instagram.com/mohamed__e__ahmed?igshid=YmMyMTA2M2Y=">Mohamed</a>
   </p>
-      <p style={{ fontSize: '10px', marginBottom: '20px', fontFamily: '"Lucida Console", "Courier New", monospace' }}>Version: 1.0.0</p>
+      <p style={{ fontSize: '10px', marginBottom: '20px', fontFamily: '"Lucida Console", "Courier New", monospace' }}>Version: 1.0.1</p>
       </header>
     </div>
     </div>
